b38b5517d99ce0a64592e1f995e5eb3dd9b41f62
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2023-10-11 03:34:29 +0600

    ok

--- math.js
+++ math.js
@@ -1,2 +1,25 @@
 const result=Math.pow(3,8);
-console.log(result);
+// console.log(result);
+
+const num1=25;
+const num2=45;
+const gap=Math.abs(num1-num2);
+console.log(gap);
+if(gap<5){
+    console.log('you guys can be friends');
+}
+else{
+    console.log('you guys stay apart');
+}
+
+// ------------------
+
+const number=2.453;
+const fullNumber=Math.round(number);
+// console.log(fullNumber);
+const result1=Math.ceil(2.453);
+// console.log(result1);
+
+// -----------------
+// random
+// console.log(Math.random());
